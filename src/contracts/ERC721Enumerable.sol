e10c3bc418f1a4f698b30315bbb10b751546c981
Gabriele Dolfi <gabrieledolfi@Mac-mini-di-Gabriele.local>
2023-01-20 17:20:57 +0100

    add all the funtions in enumeration.sol

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -8,40 +8,49 @@ contract ERC721Enumerable is ERC721 {
     uint256[] private _allTokens;
 
     // mapping from tokenId to position in _allTokens array
-        mapping(uint256 => uint256) private _allTokensIndex;
+    mapping(uint256 => uint256) private _allTokensIndex;
 
     // mapping of owner to list[] of all owner tokens ids
-        mapping(address => uint256[]) private _ownedTokens;
+    mapping(address => uint256[]) private _ownedTokens;
 
     // mapping from token ID to index of the owner tokens list    
-        mapping(uint256 => uint256) private _ownedTokensIndex;
-
-   
-    ///  (sort order not specified)
-    //function tokenByIndex(uint256 _index) external view returns (uint256){}
-
+    mapping(uint256 => uint256) private _ownedTokensIndex;
     
-    ///   (sort order not specified)
-    //function tokenOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256){}
-
     function _mint(address to, uint256 tokenId) internal override(ERC721){
         //super for calling parent contract
         super._mint(to, tokenId);
-
         //add tokens to the owner
         //all tokens to our totalsupply - to allTokens
-
-        _addTokensToTotalSupply(tokenId);
-
+        _addTokensToAllTokenEnumeration(tokenId);
+        _addTokensToOwnerEnumeration(to, tokenId);
     }
 
-    function _addTokensToTotalSupply(uint256 tokenId) private {
+    //add tokens to the _allTokens array and set the possition of the tokens indexes
+    function _addTokensToAllTokenEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
         _allTokens.push(tokenId);
     }
 
-    /// @notice Count NFTs tracked by this contract
-    /// @return A count of valid NFTs tracked by this contract, where each one of
-    ///  them has an assigned and queryable owner not equal to the zero address
+    //add address and token id to the _owned Tokens
+    function _addTokensToOwnerEnumeration(address to, uint256 tokenId) private {
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
+        _ownedTokens[to].push(tokenId);
+    }
+
+    // return tokenByIndex
+    function tokenByIndex(uint256 index) public view returns(uint256) {
+        // check index is not out of bound of the supply total
+        require(index < totalSupply(), 'global index is out of bound');
+        return _allTokens[index];
+    }
+
+    // return tokenOfOwnerByIndex
+    function tokenOfOwnerByIndex(address owner, uint index) public view returns(uint256) {
+         require(index < this.balanceOf(owner), 'owner index is out of bound');
+         return _ownedTokens[owner][index];
+    }
+
+    // return the total supply of the _allTokens array
     function totalSupply() public view returns(uint256){
         return _allTokens.length;
     }
